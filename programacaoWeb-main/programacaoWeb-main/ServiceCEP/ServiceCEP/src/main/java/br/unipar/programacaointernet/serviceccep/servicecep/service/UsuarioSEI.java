72361bca7c88809337755be478073291b9299563
Vinicius Sempkoski <98129502+vinicius-sempkoski@users.noreply.github.com>
2024-03-18 19:29:56 -0300

    Update UsuarioSEI.java

--- programacaoWeb-main/programacaoWeb-main/ServiceCEP/ServiceCEP/src/main/java/br/unipar/programacaointernet/serviceccep/servicecep/service/UsuarioSEI.java
+++ programacaoWeb-main/programacaoWeb-main/ServiceCEP/ServiceCEP/src/main/java/br/unipar/programacaointernet/serviceccep/servicecep/service/UsuarioSEI.java
@@ -10,10 +10,15 @@ public interface UsuarioSEI {
 
     @WebMethod
     String boasVindas(@WebParam(name = "nome") String nome);
-
+    @WebMethod
     Usuario consultaUsuario(@WebParam(name = "idUsuario") Long idUsuario);
+    @WebMethod
     String salvarUsuario(@WebParam(name = "nome") String nome, @WebParam(name = "login") String login, @WebParam(name = "senha") String senha);
+    @WebMethod
     String editarUsuario(@WebParam(name = "idUsuario") Long idUsuario, @WebParam(name = "senha") String senha);
+    @WebMethod
     String deletarUsuario(@WebParam(name = "idUsuario") Long idUsuario);
+    @WebMethod
+    List<Usuario> consultaUsuarios();
 
 }
